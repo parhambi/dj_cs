9473d72d2928de88974d1c0ae4bd8ecbcaa78815
parhambi <parhambakhtiyari.pb@gmail.com>
2024-12-09 07:04:06 -1200

    new app created called tasks

--- cs_go/tasks/views.py
+++ cs_go/tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+todos = [
+    "create music",
+    "make money",
+    "learn django"
+]
+def index(request):
+    return render(request,"tasks/index.html",{"todos":todos})
