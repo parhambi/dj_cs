772076bd883977d4a01d560340d0695d428d9b92
parhambi <parhambakhtiyari.pb@gmail.com>
2024-12-09 07:43:27 -1200

    css

--- cs_go/hello/urls.py
+++ cs_go/hello/urls.py
@@ -1,8 +1,9 @@
 from . import views
-from django.urls import path
+from django.urls import path,include
+app_name = "hello"
 urlpatterns = [
     path("",views.index,name="index"),
     path("greet/<str:name>/",views.greet,name = "greet")
-    
+
 
 ]
