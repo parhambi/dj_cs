7c978c7e2f65db841b06c74eb71a2597807201ac
parhambi <parhambakhtiyari.pb@gmail.com>
2024-12-11 21:09:25 -1200

    end of lecture3

--- cs_go/cs_go/settings.py
+++ cs_go/cs_go/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'cs_go.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dj_cs',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- cs_go/tasks/views.py
+++ cs_go/tasks/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 todos = [
@@ -6,8 +9,23 @@ todos = [
     "make money",
     "learn django"
 ]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New task")
+    priority = forms.IntegerField(label="priority",min_value=1,max_value=20)
+
 def index(request):
-    return render(request,"tasks/index.html",{"todos":todos})
+    if  "todos" not in request.session:
+        request.session["todos"] = []
+    return render(request,"tasks/index.html",{"todos":request.session["todos"]})
 
 def add(request):
-    return render(request,"add/index.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["todos"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request,"add/index.html",{"task":form})
+    context = {"task":NewTaskForm()}
+    return render(request,"add/index.html",context)
